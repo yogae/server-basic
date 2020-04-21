0098b6a75a9a7165c0b32c1bfbdb0bfeeeac6a91
kim jihyun <>
2020-04-21 23:28:58 +0900

    add homework

--- aws-sdk/s3.py
+++ aws-sdk/s3.py
@@ -1,13 +1,14 @@
 import boto3 as aws
 
-client = aws.client('s3')
+# 사용법
+client = aws.client('s3') 
 
-def print_bucket_name():
-    response = client.list_buckets()
-    for bucket in response['Buckets']:
-        print(bucket['Name'])
+# def print_bucket_name():
+#     response = client.list_buckets()
+#     for bucket in response['Buckets']:
+#         print(bucket['Name'])
 
-print_bucket_name()
+# print_bucket_name()
 
 def get_bucket_name(num):
     response = client.list_buckets()
@@ -15,6 +16,7 @@ def get_bucket_name(num):
 
 def list_objects(bucket_name):
     list_objects = client.list_objects_v2(Bucket=bucket_name)
+    print(list_objects['Contents'])
     for obj in list_objects['Contents']:
         print(obj['Key'])
 
