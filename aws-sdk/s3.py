319c5d8c908e749ab2fc752b175e1f4b1d9b18ce
kim jihyun <>
2020-04-23 23:39:22 +0900

    add test4, test5

--- aws-sdk/s3.py
+++ aws-sdk/s3.py
@@ -1,24 +1,27 @@
 import boto3 as aws
+import botostubs
 
-# 사용법
-client = aws.client('s3') 
+s3 = aws.client('s3') # type: botostubs.S3
 
-# def print_bucket_name():
-#     response = client.list_buckets()
-#     for bucket in response['Buckets']:
-#         print(bucket['Name'])
+
+def print_bucket_name():
+    response = s3.list_buckets()
+    for bucket in response['Buckets']:
+        print(bucket['Name'])
 
 # print_bucket_name()
 
+
 def get_bucket_name(num):
-    response = client.list_buckets()
+    response = s3.list_buckets()
     return response['Buckets'][num]['Name']
 
+
 def list_objects(bucket_name):
-    list_objects = client.list_objects_v2(Bucket=bucket_name)
-    print(list_objects['Contents'])
+    list_objects = s3.list_objects_v2(Bucket=bucket_name)
     for obj in list_objects['Contents']:
         print(obj['Key'])
+
 
 bucket_name = get_bucket_name(0)
 list_objects(bucket_name)
